31ba72ac11fa97f3ca9944160aa8daf6c037c81e
BARarch <anthonyquivers@gmail.com>
2017-06-14 18:26:47 -0400

    problem set 2 number 3

---
+++ ps00203.py
@@ -0,0 +1,35 @@
+def lowestPaymentIter(low, high, balance, interest):
+    if balanceYear(balance, (high + low) / 2, interest) > .001:
+        ## Mid Payment too Low, Value is between Mid and High
+        print("High: " + str((high + low) / 2))
+        return lowestPaymentIter((high + low) / 2, high, balance, interest)
+    elif balanceYear(balance, (high + low) / 2, interest) < -.001:
+        ## Mid Payment too High, Value is between Low and Mid
+        print("Low: "+ str((high + low) / 2))
+        return lowestPaymentIter(low, (high + low) / 2, balance, interest)
+
+    else:
+        return (high + low) / 2
+
+def lowestPaymentIter2(low, high, balance, interest):
+    if round(high, 2) == round(low, 2):
+        return round(high,2)
+    elif balanceYear(balance, (high + low) / 2, interest) > 0:
+        ## Mid Payment too Low, Value is between Mid and High
+        ##print("Low: " + str((high + low) / 2))
+        return lowestPaymentIter2((high + low) / 2, high, balance, interest)
+    else:
+        ## Mid Payment too High, Value is between Low and Mid
+        ##print("High: "+ str((high + low) / 2))
+        return lowestPaymentIter2(low, (high + low) / 2, balance, interest)
+    
+
+def lowestPayment(balance, interest):
+    monthlyInterest = interest / 12
+    low0 = balance / 12
+    high0 = (balance * (1 + monthlyInterest)**12) / 12
+    return lowestPaymentIter2(low0, high0, balance,interest)
+
+balance = 999999
+annualInterestRate = .18
+print("Lowest Payment: " + str(lowestPayment(balance, annualInterestRate)))   
