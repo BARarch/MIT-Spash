ef224db48df70b5084fdef2e65ee4cd852357bce
Anthony Quivers <anthonyquivers@gmail.com>
2017-07-09 22:00:42 -0400

    game play implemted

--- ts4/ps4a.py
+++ ts4/ps4a.py
@@ -269,22 +269,36 @@ def playHand(hand, wordList, n):
 def playGame(wordList):
     """
     Allow the user to play an arbitrary number of hands.
-
+ 
     1) Asks the user to input 'n' or 'r' or 'e'.
       * If the user inputs 'n', let the user play a new (random) hand.
       * If the user inputs 'r', let the user play the last hand again.
       * If the user inputs 'e', exit the game.
       * If the user inputs anything else, tell them their input was invalid.
  
-    2) When done playing the hand, repeat from step 1    
+    2) When done playing the hand, repeat from step 1
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
-   
+    hand = {}
+    
+    while True:
+        userCmd = input('Enter n to deal a new hand, r to replay the last hand, or e to end game: ')
 
-
-
-#
+        if userCmd == 'n':
+            hand = dealHand(HAND_SIZE)
+        elif userCmd == 'r':
+            if not hand:
+                print('You have not played a hand yet. Please play a new hand first!')
+                print()
+                continue
+        elif userCmd == 'e':
+            break
+        else:
+            print('Invalid command.')
+            continue
+            
+        playHand(hand, wordList, HAND_SIZE)
+        print()
+        
 # Build data structures used for entire session and play game
 #
 if __name__ == '__main__':
